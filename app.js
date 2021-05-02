0daa96289d78679f0c7347c3d3e0d4aeafd6dd93
edy0313 <81029563+edy0313@users.noreply.github.com>
2021-05-02 21:55:23 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-// クラス名から要素を選択
-const hamburger = document.querySelector('.hamburgerMenu');
-const link = document.querySelector('.link');
+// ここからハンバーガー開閉JS
+// jQuery無しでナビゲーションを開閉する関数
+function navToggle() {
 
-// ハンバーガーメニューにイベントをつける
-hamburger.addEventListener('click',() => {
-  // toggle('クラス名')：クラス名があれば削除・なければ追加
-  link.classList.toggle('open');
+  // 開閉ボタンを取得
+  var toggleBtn = document.getElementById('nav-toggle');
 
-})
+  // 開閉するナビゲーション本体を取得
+  var navView = document.getElementById('nav-list');
 
+  // 開閉ボタンの現在のクラスを取得
+  var toggleBtnClass = toggleBtn.getAttribute('class');
 
+  // 開閉ボタンのクラスで条件分岐
+  // 1. 開閉ボタンのクラスが「close」だったら
+  if(toggleBtnClass == 'nav-toggle-button close') {
+
+    // 閉じている状態のクラスを削除
+    toggleBtn.classList.remove('close');
+    navView.classList.remove('close');
+
+    // 開いている状態のクラスを付与
+    toggleBtn.classList.add('open');
+    navView.classList.add('open');
+
+  }
+
+  // 2. 開閉ボタンのクラスが「open」だったら
+  else {
+
+    // 開いている状態のクラスを削除
+    toggleBtn.classList.remove('open');
+    navView.classList.remove('open');
+
+    // 閉じている状態のクラスを付与
+    toggleBtn.classList.add('close');
+    navView.classList.add('close');
+  }
+// ここまでハンバーガー開閉JS
+}
+
+// 指定IDをクリックした際に関数を実行
+document.getElementById('nav-toggle').onclick = navToggle;
